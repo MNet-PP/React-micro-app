16ddd5547f8718710450a389d902860a8290450f
Mihail Nechaev <sadovni4ek@gmail.com>
2023-02-26 16:11:28 +0300

    app is completed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import Item from './Item'
-import './App.css';
-import { render } from '@testing-library/react';
+import Item from "./Item";
+import "./App.css";
 
 const item = new Item({
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  brand: "Tiger of Sweden",
+  title: "Leonard coat",
+  description: "Minimalistic coat in cotton-blend",
+  descriptionFull:
+    "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
   price: 399,
-  currency: '£'
-})
-
-
+  currency: "£",
+});
 
 function App() {
   return (
     <div className="container">
-      <div className="background-element">
-      </div>
+      <div className="background-element"></div>
       <div className="highlight-window">
-        <div className='highlight-overlay'></div>
+        <div className="highlight-overlay"></div>
       </div>
-      <div className="window">
-        {item.render()}
-      </div>
+      <div className="window">{item.render()}</div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-
+import ShopItemFunc from "./ShopItemFunc";
+//import PropTypes from "prop-types"
 
 export default class Item extends React.Component {
-     constructor(item) {
-        super();
-        this.brand = item.brand;
-        this.title = item.title;
-        this.description = item.description;
-        this.descriptionFull = item.descriptionFull;
-        this.price = item.price;
-        this.currency = item.currency;
-     }
-   render() {
-     
-   }
-
-
+  constructor(item) {
+    super();
+    this.brand = item.brand;
+    this.title = item.title;
+    this.description = item.description;
+    this.descriptionFull = item.descriptionFull;
+    this.price = item.price;
+    this.currency = item.currency;
+  }
+  render() {
+    return (
+      <ShopItemFunc
+        brand = {this.brand}
+        title = {this.title}
+        description ={this.description}
+        descriptionFull = {this.descriptionFull}
+        price = {this.price}
+        currency = {this.currency}
+      />
+    );
+  }
 }
+
+//Item.propTypes ={
+// optionalString: PropTypes.string,
+// optionalNumber: PropTypes.number,
+//}
